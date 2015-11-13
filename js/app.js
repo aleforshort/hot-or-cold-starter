cf499d07ad828e7c940dc64cc8b9466b0bf5afbc
Alejandra Diaz <diaz.rivera.alejandra@gmail.com>
2015-11-12 21:06:52 -0500

    update

--- js/app.js
+++ js/app.js
@@ -10,8 +10,24 @@ $(document).ready(function(){
   	/*--- Hide information modal box ---*/
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
-  	});
 
-});
+  	function newGame() {
+  		var userInput = $("#userGuess").val()
+      $(".new").on("click", function (){
+          userInput.delete;
+      }
+
+  	function startGame() { 
+      $(."guessBox")on("click", function(){
+        function getRandom(min, max) {
+        return Math.random() ;
+          getRandom (1, 100)
+
+    }  
+      }
+       }
+        }
+  	     });
+          });
 
 
