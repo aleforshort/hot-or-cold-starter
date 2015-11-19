faf56aaddd4183e822f4ab14196198afbe3213fd
Alejandra Diaz <diaz.rivera.alejandra@gmail.com>
2015-11-18 20:06:04 -0500

    update

--- js/app.js
+++ js/app.js
@@ -10,24 +10,41 @@ $(document).ready(function(){
   	/*--- Hide information modal box ---*/
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
+    });
 
   	function newGame() {
-  		var userInput = $("#userGuess").val()
-      $(".new").on("click", function (){
-          userInput.delete;
-      }
+  		// var userInput = $("#userGuess").val()
+      var randomNumber = getRandom (1, 100);
+      $("#guessList").append("<li>"+randomNumber+"</li");
+    }
 
   	function startGame() { 
-      $(."guessBox")on("click", function(){
-        function getRandom(min, max) {
-        return Math.random() ;
-          getRandom (1, 100)
-
-    }  
-      }
-       }
+      var userInput = $("#userGuess") .val();
+       if (userInput == randomNumber){
+        $("#feedback").append("<h2>You win</h2>");
         }
+        else if (userInput == randomNumber - 20 && userInput == randomNumber + 20) {
+          $("#feedback").append("<h2>Hot</h2>");
+        }
+
+        else{
+          $("#feedback").append("<h2>Cold</h2>")
+        }
+
+    }
+      function getRandom(min, max) {
+       return Math.floor(Math.random() * (max - min)) + min;
+          
+      }
+
+      $(".guessBox").on("click", function(){
+        
+        
+
   	     });
-          });
+    $(".new").on("click", newGame);
+
+              
 
 
+});
